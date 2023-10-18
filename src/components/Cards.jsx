e7591624b9d9aa88d2855f133a3e7b2b15e5342a
mays alwattar <maysmm199@gmail.com>
2023-10-18 05:11:50 +0300

    up date

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import "../styles/cards.css";
 import "../styles/button.css";
+import alarmclock from "../assets/alarmclock.png";
+import view from "../assets/view.png";
+import heart from "../assets/heart.png";
 export const Cards = ({ imgSrc, imgAlt, title }) => {
   return (
     <div className="card-container">
       {imgSrc && imgAlt && (
         <img src={imgSrc} alt={imgAlt} className="cardImg" />
       )}
+      <div className="card-container-heart">
+        <img src={heart} height={30} alt="heart"/>
+      </div>
+
       {title && <h3 className="card-title">{title}</h3>}
-      <div 
-      className="buttos">
-        <a href="/Details/Details-continer">
-        <button className="buttos-btn"> عرض التفاصيل</button>
-        </a>
-        <button className="buttos-btn"> اضافة منبه</button>
+
+      <div className="buttos">
+        <div className="al-btn">
+          <div className="view-icon"></div>
+          <div className="al-bb">
+            <a href="/Details/Details-continer">
+              <button className="buttos-btn">
+                {" "}
+                <img src={view} height={30} alt="view"></img>
+                عرض التفاصيل
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div className="al-btn">
+          <div className="alarm-icon"></div>
+          <div className="al-bb">
+            <button className="buttos-btn">
+              {" "}
+              <img src={alarmclock} height={38} alt="alarmclock"></img>
+              اضافة منبه
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
